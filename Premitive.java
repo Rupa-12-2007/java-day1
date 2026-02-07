579f7f621ea54a83c8e42924ade458b382cfabfb
Rupa-12-2007 <rkotholla12@gmail.com>
2026-02-07 11:40:03 +0530

    Add files via upload

---
+++ Premitive.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Premitive{
+    public static void main(String[] args)
+    {
+        int sci=67;
+        int math=100;
+        int hin=86;
+        int sum=sci+math+hin;
+        float ff=sum/3;
+        system.out.println(ff);
+        System.out.println("sum");
+    }    
+}
