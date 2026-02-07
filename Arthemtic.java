7680b722957c79e33dc2e9757ef200956c4c6658
Rupa-12-2007 <rkotholla12@gmail.com>
2026-02-07 14:03:05 +0530

    Add files via upload

---
+++ Arthemtic.java
@@ -0,0 +1,12 @@
+public class Arthemtic {
+    public static void main(String []args)
+    {
+    int a=8;
+    int b=12;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+    }
+}
